1e6d4c1a325f74fa3b51c7bbadbd8f09cdc0babf
Ataxia123 <vp.ca94@gmail.com>
2023-07-11 00:06:36 -0500

    Deploy website - based on 2979682a384eca9f0bf26df53933cc916f484d1d

---
+++ assets/js/0415da5a.8edc2ca3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknerd_website=self.webpackChunknerd_website||[]).push([[544],{6454:e=>{e.exports=JSON.parse('{"label":"Intellectual_property","permalink":"/nerddao/blog/tags/intellectual-property","allTagsPath":"/nerddao/blog/tags","count":1}')}}]);
---
+++ assets/js/3deb5e81.45182d6c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknerd_website=self.webpackChunknerd_website||[]).push([[9211],{2341:e=>{e.exports=JSON.parse('{"permalink":"/nerddao/blog/tags/intellectual-property","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
